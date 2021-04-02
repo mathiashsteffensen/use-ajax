c05f62d871acbaef3ecdc513e66e5fabffdfbb77
mathiashsteffensen <mathiashsteffensen@protonmail.com>
2021-04-02 20:13:42 +0200

    Added TS declaration file

---
+++ use-ajax.d.ts
@@ -0,0 +1,7 @@
+type AjaxFunction = () => Promise<any>
+
+type Options = { timeout?: number, runOnce?: boolean }
+
+type Returns = { data: any, error: any, loading: boolean }
+
+declare function useAJAX(ajaxFunction: AjaxFunction, args: Array<any>, options: Options): Returns
